a906ff7fbb2f40b3df1fb2e21c4c7e1d0e31e97f
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-11-27 20:07:21 +0100

    First commit

---
+++ 02_python/listy.py
@@ -0,0 +1,12 @@
+wartosc = 123
+lista = [
+    True, False, None,
+    -1, 3.14, 5+6j,
+    "Piotr", [],
+]
+lista[5] = -wartosc
+tekst = "Piotr"
+# tekst[3] = "T"
+print(lista)
+print(lista[3::2])
+# print(lista[-4:-1:1])
