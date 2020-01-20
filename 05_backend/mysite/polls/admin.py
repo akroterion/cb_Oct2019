04154a011a72bde45226590828bd51bbfded175b
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-01-20 20:50:10 +0100

    Aktualizacja

--- 05_backend/mysite/polls/admin.py
+++ 05_backend/mysite/polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+# Dodaj:
+# - wyszukiwanie
+# - filtrowanie po dacie
+# - filtrowanie po pozostałych polach
+# - pobrania zależnych modeli
+# - sortowanie
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+
+
+# - autouzupełnianie
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
