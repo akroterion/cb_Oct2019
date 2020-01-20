f4d86dc05ac2b34d3cd302a43325bb003d1f4a01
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-01-20 20:50:10 +0100

    Aktualizacja

--- 05_backend/mysite/polls/views.py
+++ 05_backend/mysite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -24,8 +25,21 @@ def details(request, question_id):
 
 # /polls/<question_id>/vote/
 def vote(request, question_id):  # metoda POST
-    pass
+    q1 = get_object_or_404(Question, pk=question_id)
 
+    try:
+        c1 = q1.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/details.html", {
+            'title': q1.question_text,
+            'question': q1,
+            'error_message': "Nie wybrałeś odpowiedzi!",
+        })
+    else:
+        c1.votes += 1
+        c1.save()
+        return redirect(reverse('results', args=[q1.pk]))
+        # return redirect('results', question_id=q1.pk)
 
 # /polls/<question_id>/results/
 def results(request, question_id):
